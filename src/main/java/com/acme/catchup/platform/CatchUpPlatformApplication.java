afa6595b020f53bc8f8be9c65c5b323879351ec1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-20 21:14:39 -0500

    refactor(application): change main method visibility to static

--- src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
+++ src/main/java/com/acme/catchup/platform/CatchUpPlatformApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class CatchUpPlatformApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(CatchUpPlatformApplication.class, args);
     }
 
