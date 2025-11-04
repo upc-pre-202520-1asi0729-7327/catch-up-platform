8c35a437deb84c3ab5fca37423d4894f0656646c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-03 21:46:29 -0500

    feat(logging): add logging for duplicate favorite source entries.

--- src/main/java/com/acme/catchup/platform/news/application/internal/commandservices/FavoriteSourceCommandServiceImpl.java
+++ src/main/java/com/acme/catchup/platform/news/application/internal/commandservices/FavoriteSourceCommandServiceImpl.java
@@ -4,22 +4,31 @@ import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
 import com.acme.catchup.platform.news.domain.model.commands.CreateFavoriteSourceCommand;
 import com.acme.catchup.platform.news.domain.services.FavoriteSourceCommandService;
 import com.acme.catchup.platform.news.infrastructure.persistence.jpa.FavoriteSourceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class FavoriteSourceCommandServiceImpl implements FavoriteSourceCommandService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FavoriteSourceCommandServiceImpl.class);
     private final FavoriteSourceRepository favoriteSourceRepository;
+    private final MessageSource messageSource;
 
-    public FavoriteSourceCommandServiceImpl(FavoriteSourceRepository favoriteSourceRepository) {
+    public FavoriteSourceCommandServiceImpl(FavoriteSourceRepository favoriteSourceRepository, MessageSource messageSource) {
         this.favoriteSourceRepository = favoriteSourceRepository;
+        this.messageSource = messageSource;
     }
 
     @Override
     public Optional<FavoriteSource> handle(CreateFavoriteSourceCommand command) {
-        if (favoriteSourceRepository.existsByNewsApiKeyAndSourceId(command.newsApiKey(), command.sourceId()))
-            throw new IllegalArgumentException("Favorite source already exists for the given newsApiKey and sourceId");
+        if (favoriteSourceRepository.existsByNewsApiKeyAndSourceId(command.newsApiKey(), command.sourceId())) {
+            LOGGER.error(messageSource.getMessage("favorite.source.error.duplicate", null, LocaleContextHolder.getLocale()));
+            return Optional.empty();
+        }
         var favoriteSource = new FavoriteSource(command);
         var createdFavoriteSource = favoriteSourceRepository.save(favoriteSource);
         return Optional.of(createdFavoriteSource);
